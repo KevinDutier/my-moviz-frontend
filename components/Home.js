e7005392c753829f54c36e1996f63b8e2481d3fe
KevinDutier desktop <kevin.dutier@gmail.com>
2022-12-02 12:15:23 +0100

    initial commit

--- components/Home.js
+++ components/Home.js
@@ -5,6 +5,7 @@ import { faCircleXmark } from '@fortawesome/free-solid-svg-icons';
 import Movie from './Movie';
 import 'antd/dist/antd.css';
 import styles from '../styles/Home.module.css';
+import { BACKEND_ADDRESS } from "../environmentVariables";
 
 function Home() {
   const [likedMovies, setLikedMovies] = useState([]);
@@ -37,7 +38,7 @@ function Home() {
   // Movies list
 
   useEffect(() => {
-    fetch("https://my-moviz-backend-jgkk.vercel.app/movies")
+    fetch(`${BACKEND_ADDRESS}/movies`)
     .then(res => res.json())
     .then(data => {
       setMoviesData(data.movies.map(movie => {
@@ -51,14 +52,6 @@ function Home() {
       }));
     })
   }, [])
-
-  // const moviesData = [
-  //   { title: 'Forrest Gump', poster: 'forrestgump.jpg', voteAverage: 9.2, voteCount: 22_705, overview: 'A man with a low IQ has accomplished great things in his life and been present during significant historic events—in each case.' },
-  //   { title: 'The Dark Knight', poster: 'thedarkknight.jpg', voteAverage: 8.5, voteCount: 27_547, overview: 'Batman raises the stakes in his war on crime and sets out to dismantle the remaining criminal organizations that plague the streets.' },
-  //   { title: 'Your name', poster: 'yourname.jpg', voteAverage: 8.5, voteCount: 8_691, overview: 'High schoolers Mitsuha and Taki are complete strangers living separate lives. But one night, they suddenly switch places.' },
-  //   { title: 'Iron Man', poster: 'ironman.jpg', voteAverage: 7.6, voteCount: 22_7726, overview: 'After being held captive in an Afghan cave, billionaire engineer Tony Stark creates a unique weaponized suit of armor to fight evil.' },
-  //   { title: 'Inception', poster: 'inception.jpg', voteAverage: 8.4, voteCount: 31_546, overview: 'Cobb, a skilled thief who commits corporate espionage by infiltrating the subconscious of his targets is offered a chance to regain his old life.' },
-  // ];
 
   const movies = moviesData.map((data, i) => {
     const isLiked = likedMovies.some(movie => movie === data.title);
